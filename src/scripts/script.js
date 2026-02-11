073bcf8e47233651fd32656875de200e00d684c3
diego1999dd <diego.9090dd@gmail.com>
2026-02-11 00:41:57 -0300

    primeiros passo com o gsap, animei a Hero, e outros textos da pagina, junto das divs

---
+++ src/scripts/script.js
@@ -0,0 +1,9 @@
+// SCROLL SUAVE -> ANIMAÇÕES COM SCROLLTRIGGER E SPLITTEXT
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText)
+
+
+ScrollSmoother.create({
+    smooth: 1.5,
+    effects: true
+})
+
